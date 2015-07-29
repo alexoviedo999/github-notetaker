2b33f0149a56c5e574f58f2e12210c82287fee56
alexoviedo <alexoviedo999@gmail.com>
2015-07-28 22:59:33 -0400

    started e6 refactor.

--- app/App.js
+++ app/App.js
@@ -1,12 +1,12 @@
-var React = require('react');
-var Router = require('react-router');
-var routes = require('./config/routes');
+import React from 'react';
+import Router from 'react-router';
+import routes from './config/routes';
 
 // enables react dev tools in chrome:
 if (typeof window !== 'undefined') {
     window.React = React;
 }
 
-Router.run(routes, function(Root){
-	React.render(<Root />, document.getElementById('app'));
+Router.run(routes, (Root, state) => {
+	React.render(<Root {...state} />, document.getElementById('app'));
 });
